4face6bcbe903e8c44ee1034996a625ce39bff57
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-02-25 19:20:56 +0100

    Updated configuration for vercel

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -15,5 +15,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
 
-app = application
+app = application  # Defined for deployment in vercel.
+
 
