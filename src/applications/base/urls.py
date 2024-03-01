be5acc30cfd42b46f5c4e24d95ee511da29e0659
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-01 05:43:10 +0100

    Updated base urls

--- src/applications/base/urls.py
+++ src/applications/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from applications.base.views import HomeView
+from applications.base.views import HomeView, MessageView
 
 app_name = "base"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("messages/", MessageView.as_view(), name="messages"),
 ]
