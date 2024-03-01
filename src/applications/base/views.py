8d19c095e4a090bd8e16707d9d43a2578bc1283a
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-01 05:42:36 +0100

    Updated views

--- src/applications/base/views.py
+++ src/applications/base/views.py
@@ -2,4 +2,13 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = f"base/home.html"
+    template_name = "base/home.html"
+
+
+class MessageView(TemplateView):
+    template_name = "base/message.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.request.GET.get("title", "You Got the Following Messages")
+        return context
--- src/applications/users/views.py
+++ src/applications/users/views.py
@@ -3,7 +3,6 @@ from django.views.generic import DetailView, UpdateView
 from django.contrib.auth import get_user_model
 from applications.users.constants import APP_NAME
 from applications.users.mixins import UserIsOwnerMixin
-from django.http import Http404
 
 
 class ProfileView(LoginRequiredMixin, DetailView):
