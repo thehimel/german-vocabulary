ac3d5baef23bced0cda40d9eb8622f0ebfbda60d
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-03 20:46:24 +0100

    added docstring in Language model

--- src/apps/words/models.py
+++ src/apps/words/models.py
@@ -5,6 +5,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Language(models.Model):
+    """
+    During adding a language, it shows all the available languages from settings where the list of languages are saved
+    in a pair of (code, name) i.e. ('en', 'English').
+
+    A dictionary is created from this list of tuple to return the corresponding name during string representation of the
+    object specially helpful in the list of this model in the admin panel.
+
+    The 'slug' field is not editable and is read only in detail view in the admin panel only during the update view.
+    """
+
     languages, code_choices = dict(), list()
     for pt1, name in settings.LANGUAGES:
         language = (pt1, _(name))
