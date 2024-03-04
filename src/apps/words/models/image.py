05357a67d5c828d2ead2ca37d1f189b35f6a6910
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-04 02:38:56 +0100

    updated string representation

--- src/apps/words/models/image.py
+++ src/apps/words/models/image.py
@@ -6,3 +6,6 @@ from apps.base.utils.validators import validate_file_size
 class Image(models.Model):
     image = models.ImageField(default=DEFAULT_WORD_IMAGE, upload_to="words/", validators=[validate_file_size])
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.image.path.split("/media/", maxsplit=1)[1])  # The part after '/media/'
