ed6f3aa13a4ed86c58e242fa260aa5d6476199d4
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-04 22:11:57 +0100

    optimized script for flashcard

--- src/static/cards/scripts/base.js
+++ src/static/cards/scripts/base.js
@@ -1,28 +1,34 @@
 // Flip the card on click except click on the play button
 document.getElementById('flashCard').addEventListener('click', function (event) {
-  if (event.target !== document.getElementById('playButton')) {
-    // If the clicked element does not have the class 'playButton', flip the card
-    const flipAudio = document.getElementById('flipAudio');
-    document.getElementById('flashCard').classList.toggle('rotate');
+  const flashCard = document.getElementById('flashCard');
+  const playButton = document.getElementById('playButton');
+  const flipAudio = document.getElementById('flipAudio');
+
+  if (event.target !== playButton) {
+    flashCard.classList.toggle('rotate');
     flipAudio.play();
   }
 });
 
 function changeText() {
-  const button = document.getElementById('playButton');
-  button.innerHTML = 'Playing...';
-  speakText('Ich bin sehr glücklich.', 'de-DE', 0.7); // German language code: "de-DE"
+  const playButton = document.getElementById('playButton');
+  let text = 'Ich bin sehr glücklich.'
+  let lang = 'de-DE'
+  let speed = 0.6
+
+  playButton.innerHTML = 'Playing...';
+  speakText(text, lang, speed);
 }
 
 function speakText(text, lang) {
-  const speechSynthesis = window.speechSynthesis; // Using the Web Speech API for text-to-speech
+  const speechSynthesis = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance(text);
 
   utterance.lang = lang;
 
   utterance.onend = function () {
-    const button = document.getElementById("playButton");
-    button.innerHTML = "Play"; // Change button text when TTS is complete
+    const playButton = document.getElementById("playButton");
+    playButton.innerHTML = "Play"; // Change button text when TTS is complete
   };
 
   speechSynthesis.speak(utterance);
@@ -36,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // If it's a refresh, add the 'loaded' class immediately
     fadeContainer.classList.add('loaded');
   } else {
-    // If it's the first load, add the 'loaded' class after 1 second
+    // If it's the first load, add the 'loaded' class after 1/2 second
     setTimeout(function () {
       fadeContainer.classList.add('loaded');
     }, 500);
