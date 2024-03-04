0ec9ec6323303df94a42b42e9667232e5f112659
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-04 21:46:52 +0100

    play audio

--- src/static/cards/scripts/base.js
+++ src/static/cards/scripts/base.js
@@ -1,9 +1,11 @@
-function rotateCard(cardContainer) {
-  const flashcard = cardContainer.querySelector('.flashcard');
-  const flip_card = document.getElementById('flip_card');
-  flashcard.classList.toggle('rotate');
-  flip_card.play();
-}
+document.getElementById('flashCard').addEventListener('click', function (event) {
+  if (!event.target.classList.contains('play-button')) {
+    // If the clicked element does not have the class 'play-button', flip the card
+    const flip_card = document.getElementById('flip_card');
+    document.getElementById('flashCard').classList.toggle('rotate');
+    flip_card.play();
+  }
+});
 
 document.addEventListener('DOMContentLoaded', function () {
   const fadeContainer = document.querySelector('.fade-in-container');
@@ -22,3 +24,23 @@ document.addEventListener('DOMContentLoaded', function () {
     sessionStorage.setItem('isPageLoaded', 'true');
   }
 });
+
+function changeText() {
+  const button = document.getElementById("myButton");
+  button.innerHTML = "Playing...";
+  speakText("Ich bin sehr glücklich.", "de-DE", 0.7); // German language code: "de-DE"
+}
+
+function speakText(text, lang) {
+  const speechSynthesis = window.speechSynthesis; // Using the Web Speech API for text-to-speech
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  utterance.lang = lang;
+
+  utterance.onend = function () {
+    const button = document.getElementById("myButton");
+    button.innerHTML = "Play"; // Change button text when TTS is complete
+  };
+
+  speechSynthesis.speak(utterance);
+}
