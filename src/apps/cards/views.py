7b101404230060be15c8347ac0d29a95f9d8703b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-05 19:27:39 +0100

    added next and previous functionalities to the flash card

--- src/apps/cards/views.py
+++ src/apps/cards/views.py
@@ -1,4 +1,9 @@
+from django.views import View
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import TemplateView, DetailView
+
 from apps.words.models import Word
 
 
@@ -11,3 +16,26 @@ class CardDetailView(DetailView):
     model = Word
     context_object_name = 'object'
     slug_field = "pk"
+
+
+class NextCardView(View):
+    @staticmethod
+    def get(request, action='next', slug=None):
+        current_word = get_object_or_404(Word, pk=slug) if slug else None
+
+        if current_word:
+            if action == 'next':
+                next_card = Word.objects.filter(pk__gt=current_word.pk).order_by('pk').first()
+                if not next_card:
+                    messages.warning(request, _('This is the last word in this section.'))
+            elif action == 'previous':
+                next_card = Word.objects.filter(pk__lt=current_word.pk).order_by('-pk').first()
+                if not next_card:
+                    messages.warning(request, _('This is the first word in this section.'))
+            else:
+                next_card = Word.objects.order_by('?').first()
+
+            if next_card:
+                slug = next_card.pk
+
+        return redirect('cards:detail', slug=slug)
