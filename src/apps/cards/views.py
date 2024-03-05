9a127d7d85bf636cbdacd6084b6b42f6be096e77
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-05 20:12:39 +0100

    refactored view for next card

--- src/apps/cards/views.py
+++ src/apps/cards/views.py
@@ -14,29 +14,39 @@ class HomeView(TemplateView):
 class CardDetailView(DetailView):
     template_name = "cards/detail.html"
     model = Word
-    context_object_name = 'object'
+    context_object_name = "object"
     slug_field = "pk"
 
 
 class NextCardView(View):
-    @staticmethod
-    def get(request, action='next', slug=None):
-        current_word = get_object_or_404(Word, pk=slug) if slug else None
-        next_card = None
 
-        if current_word:
-            if action == 'next':
-                next_card = Word.objects.filter(pk__gt=current_word.pk).order_by('pk').first()
-                if not next_card:
-                    messages.warning(request, _('This is the last word in this section.'))
-            elif action == 'previous':
-                next_card = Word.objects.filter(pk__lt=current_word.pk).order_by('-pk').first()
-                if not next_card:
-                    messages.warning(request, _('This is the first word in this section.'))
-        else:
-            next_card = Word.objects.order_by('?').first()
+    @staticmethod
+    def get_random_word():
+        return Word.objects.order_by("?").first()
+
+    @staticmethod
+    def get_next_word(request, action, current_word):
+        next_card = None
+        if action == "next":
+            next_card = Word.objects.filter(pk__gt=current_word.pk).order_by("pk").first()
+            if not next_card:
+                messages.warning(request, _("This is the last word in this section."))
+        elif action == "previous":
+            next_card = Word.objects.filter(pk__lt=current_word.pk).order_by("-pk").first()
+            if not next_card:
+                messages.warning(request, _("This is the first word in this section."))
+        return next_card
+
+    def get(self, request, action="next", slug=None):
+        current_word = get_object_or_404(Word, pk=slug) if slug else None
+        next_card = (
+            self.get_next_word(request=request, action=action, current_word=current_word)
+            if current_word
+            else self.get_random_word()
+        )
 
         if next_card:
             slug = next_card.pk
 
-        return redirect('cards:detail', slug=slug)
+        # Redirect to home if no data in the table for this model.
+        return redirect("cards:detail", slug=slug) if slug else redirect("base:home")
