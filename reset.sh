f37efb48185059ab9b33c31b25b4bedad244fe80
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-06 05:08:19 +0100

    updated reset script

--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
-find . -name "db.sqlite3" -exec rm -f {} +
-find . -path "*/migrations/*.py" -not -name "__init__.py" -exec rm -f {} +
+find src -name "db.sqlite3" -exec rm -f {} +
+find src -path "*/migrations/*.py" -not -name "__init__.py" -exec rm -f {} +
