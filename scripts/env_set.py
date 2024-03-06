5d955b134128a15e19eebfae4bd5ea029556855b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-06 18:15:08 +0100

    updated script to set environment

--- scripts/env_set.py
+++ scripts/env_set.py
@@ -4,7 +4,7 @@ from files import read_data, write_data
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Select environment in the .env file.")
     parser.add_argument("--file_path", dest="file_path", default=".env", help="Environment file path.")
-    parser.add_argument("--env", dest="env", required=True, help="Selected environment. Options: DEV, PROD")
+    parser.add_argument("--env", dest="env", required=True, choices=["DEV", "PROD"], help="Environment to set.")
 
     args = parser.parse_args()
     file_path = args.file_path
