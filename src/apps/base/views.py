bcf7d8e7079fd9f587e66356e5a08b5432d6b8ef
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-06 06:03:07 +0100

    updated welcome view

--- src/apps/base/views.py
+++ src/apps/base/views.py
@@ -5,12 +5,6 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "base/welcome.html"
 
-    def get(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect("cards:any")
-        else:
-            return super().get(request, *args, **kwargs)
-
 
 class MessageView(TemplateView):
     template_name = "base/message.html"
