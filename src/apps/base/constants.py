ee48c66fa606a8584b14028fedda0ca824460131
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-09 00:23:00 +0100

    added max age for cookies

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -1,3 +1,4 @@
 SELECTED_LANGUAGE = "selected_language"
 PRIMARY_LANGUAGE = "primary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
+COOKIE_EXPIRATION_DAYS = 30
--- src/apps/base/context_processors.py
+++ src/apps/base/context_processors.py
@@ -1,10 +1,11 @@
 from django.http import HttpRequest
+
 from apps.base.constants import IS_LANGUAGES_SELECTED
 from apps.base.utils.languages import is_languages_selected
 
 
 def base_context(request: HttpRequest):
     return {
-        'brand_name': 'German Vocabulary',
+        "brand_name": "German Vocabulary",
         IS_LANGUAGES_SELECTED: is_languages_selected(request=request),
     }
