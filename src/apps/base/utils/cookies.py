88f15309f0242bb781eaa50c54776b9f1757763a
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-09 00:34:14 +0100

    updated constant name for cookies max age

--- src/apps/base/utils/cookies.py
+++ src/apps/base/utils/cookies.py
@@ -1,6 +1,6 @@
 from apps.base.utils.utils import get_timestamp_from_time_delta
-from apps.base.constants import COOKIE_EXPIRATION_DAYS
+from apps.base.constants import COOKIES_MAX_AGE_IN_DAYS
 
 
 def get_cookie_max_age():
-    return get_timestamp_from_time_delta(days=COOKIE_EXPIRATION_DAYS)
+    return get_timestamp_from_time_delta(days=COOKIES_MAX_AGE_IN_DAYS)
