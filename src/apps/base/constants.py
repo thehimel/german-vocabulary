2c8f884ac10ad44c33804b685b77cafb08fcafa5
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-10 19:39:25 +0100

    updated COOKIES_MAX_AGE_IN_DAYS

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -1,5 +1,7 @@
+from decouple import config
+
 LEVEL = "level"
 SELECTED_LANGUAGE = "selected_language"
 PRIMARY_LANGUAGE = "primary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
-COOKIES_MAX_AGE_IN_DAYS = 2
+COOKIES_MAX_AGE_IN_DAYS = config("COOKIES_MAX_AGE_IN_DAYS", default=1)
