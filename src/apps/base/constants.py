b072bc3ee8a9d6e2c782ed95b005c01402b9e178
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-10 19:37:04 +0100

    updated COOKIES_MAX_AGE_IN_DAYS

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -2,4 +2,4 @@ LEVEL = "level"
 SELECTED_LANGUAGE = "selected_language"
 PRIMARY_LANGUAGE = "primary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
-COOKIES_MAX_AGE_IN_DAYS = 30
+COOKIES_MAX_AGE_IN_DAYS = 2
