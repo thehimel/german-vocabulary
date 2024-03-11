1691237c263f60fbf4d90df72df929fd8b6a34ac
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-11 20:08:19 +0100

    added env variable for DEFAULT_SELECTED_LANGUAGE

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -5,3 +5,4 @@ SELECTED_LANGUAGE = "selected_language"
 SECONDARY_LANGUAGE = "secondary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
 COOKIES_MAX_AGE_IN_DAYS = config("COOKIES_MAX_AGE_IN_DAYS", default=1)
+DEFAULT_SELECTED_LANGUAGE = config("DEFAULT_SELECTED_LANGUAGE", default=None)
