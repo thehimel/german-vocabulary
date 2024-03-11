5b81ebcf1c0e4bfdffb8854d3091b2cad67c08a7
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-11 20:15:31 +0100

    renamed DEFAULT_PRIMARY_LANGUAGE

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -5,4 +5,4 @@ PRIMARY_LANGUAGE = "primary_language"
 SECONDARY_LANGUAGE = "secondary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
 COOKIES_MAX_AGE_IN_DAYS = config("COOKIES_MAX_AGE_IN_DAYS", default=1)
-DEFAULT_SELECTED_LANGUAGE = config("DEFAULT_SELECTED_LANGUAGE", default=None)
+DEFAULT_PRIMARY_LANGUAGE = config("DEFAULT_PRIMARY_LANGUAGE", default=None)
