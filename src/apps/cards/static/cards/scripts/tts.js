a4e3111de93d998605d10ae137f48a2a5a583b2e
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-12 04:50:48 +0100

    play text-to-speech using rest api

--- src/apps/cards/static/cards/scripts/tts.js
+++ src/apps/cards/static/cards/scripts/tts.js
@@ -8,21 +8,32 @@ function togglePlayIcon() {
   playButton.classList.toggle(stopIcon);
 }
 
-function speakText(text, lang) {
-  const utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = lang;
-  utterance.onend = () => togglePlayIcon();
-  speechSynthesis.speak(utterance);
+async function speakText(api_url, text, lang) {
+  try {
+    // Assuming the API returns the audio file directly as a Blob
+    const audioBlob = await getTTSResponse(api_url, text, lang);
+
+    // Create a Blob URL for the audio
+    const audioUrl = URL.createObjectURL(audioBlob);
+
+    // Create an audio element
+    const audio = new Audio(audioUrl);
+
+    // Add an event listener to toggle the play icon when the audio playback ends
+    audio.addEventListener('ended', () => togglePlayIcon());
+
+    await audio.play();
+  } catch (error) {
+    // Handle errors from getTTSResponse or audio playback
+    console.error('Error:', error);
+  }
 }
 
-function playText(text, lang) {
+function playText(api_url, text, lang) {
   if (!playing) {
-    togglePlayIcon();
-    speakText(text, lang);
+    speakText(api_url, text, lang).then(r => togglePlayIcon());
   } else {
     stopSpeakText();
     togglePlayIcon();
   }
 }
-
-
