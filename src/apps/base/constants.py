d94440a960b7d40d8f9f36cd511c82200d172dcb
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-12 16:21:22 +0100

    added cast in COOKIES_MAX_AGE_IN_DAYS

--- src/apps/base/constants.py
+++ src/apps/base/constants.py
@@ -4,5 +4,5 @@ LEVEL = "level"
 PRIMARY_LANGUAGE = "primary_language"
 SECONDARY_LANGUAGE = "secondary_language"
 IS_LANGUAGES_SELECTED = "is_languages_selected"
-COOKIES_MAX_AGE_IN_DAYS = config("COOKIES_MAX_AGE_IN_DAYS", default=1)
+COOKIES_MAX_AGE_IN_DAYS = config("COOKIES_MAX_AGE_IN_DAYS", cast=int, default=1)
 DEFAULT_PRIMARY_LANGUAGE = config("DEFAULT_PRIMARY_LANGUAGE", default=None)
