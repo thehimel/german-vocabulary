14a029edd454c70f63ec4a3f8f1497c28a4ef128
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-13 02:43:14 +0100

    updated search bar

--- src/apps/words/static/words/scripts/base.js
+++ src/apps/words/static/words/scripts/base.js
@@ -1,3 +1,13 @@
-function clearPlaceholder() {
-    document.getElementById('search_input').removeAttribute('placeholder');
-}
+// Get the input element
+const searchInput = document.getElementById('search_input');
+
+// Add event listeners for focus and blur events
+searchInput.addEventListener('focus', function () {
+    this.setAttribute('data-placeholder', this.getAttribute('placeholder'));
+    this.removeAttribute('placeholder');
+});
+
+searchInput.addEventListener('blur', function () {
+    this.setAttribute('placeholder', this.getAttribute('data-placeholder'));
+    this.removeAttribute('data-placeholder');
+});
