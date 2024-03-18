2f3f5c966b585a0d3940d1898990042b690ef260
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-18 23:15:56 +0100

    added level component

---
+++ src/client/src/units/Selector/Level.tsx
@@ -0,0 +1,14 @@
+import {Select, SelectItem} from "@nextui-org/react";
+
+export default function Languages() {
+  return (
+    <Select
+      className="max-w-xs"
+      label="Level"
+    >
+      <SelectItem key="a1">A1</SelectItem>
+      <SelectItem key="a2">A2</SelectItem>
+      <SelectItem key="a3">A3</SelectItem>
+    </Select>
+  );
+}
