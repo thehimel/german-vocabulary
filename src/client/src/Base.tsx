7663418916ec3646b741eb08d3c54f494ce8dff1
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 00:50:34 +0100

    fixed view

--- src/client/src/Base.tsx
+++ src/client/src/Base.tsx
@@ -6,10 +6,10 @@ import Selectors from "./units/Selector/Selectors.tsx";
 
 function Base() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col">
       <NavigationBar/>
       <div className="flex flex-col h-screen justify-center items-center">
-        <div className="flex justify-between w-full max-w-screen-lg mx-auto gap-2 p-2">
+        <div className="flex justify-between w-full max-w-screen-lg mx-auto gap-2">
           <Selectors/>
         </div>
         <div className="flex justify-center items-center pb-12">
