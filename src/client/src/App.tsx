5bdd9f43f244b632cbe102e77cb3ecdb1858d3c2
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-18 22:34:46 +0100

    added navigation for pagination

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -4,6 +4,7 @@ import Cards from './units/Cards/Cards.tsx'
 import useDarkMode from "use-dark-mode";
 import { HelmetProvider } from 'react-helmet-async';
 import NavigationBar from "./units/NavigationBar/NavigationBar.tsx";
+import {Pagination} from "@nextui-org/react";
 
 function Base() {
   return (
@@ -14,6 +15,9 @@ function Base() {
           <NavigationBar/>
           <div className="flex-grow flex justify-center items-center">
             <Cards/>
+          </div>
+          <div className="flex justify-center items-center fixed bottom-0 w-full p-4 pb-8 bg-dark">
+            <Pagination showShadow color="secondary" total={10} initialPage={1} />
           </div>
         </div>
       </>
