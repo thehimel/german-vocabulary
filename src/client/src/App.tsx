cbcd45661d510a2128374c1959138e84050c35b9
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-18 02:44:27 +0100

    init og tags with helmet

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,8 +1,17 @@
-import './App.css'
+import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
+  const baseUrl = window.location.origin;
+  const imageUrl = `${baseUrl}/static/global/open-graph.jpeg`;
+
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="Your page title"/>
+        <meta property="og:description" content="Your page description"/>
+        <meta property="og:image" content={imageUrl}/>
+      </Helmet>
       <div>
         <h1>Welcome</h1>
       </div>
