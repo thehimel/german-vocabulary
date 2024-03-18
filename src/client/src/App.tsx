cf962bd202690eceb1d4aca99cbaa8f67a1f44ac
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 00:13:28 +0100

    created component for base

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,35 +1,6 @@
-import './App.css';
-import Metadata from './units/Metadata/Metadata.tsx'
-import Cards from './units/Cards/Cards.tsx'
+import Base from "./Base.tsx";
 import useDarkMode from "use-dark-mode";
-import { HelmetProvider } from 'react-helmet-async';
-import NavigationBar from "./units/NavigationBar/NavigationBar.tsx";
-import {Pagination} from "@nextui-org/react";
-import Selectors from "./units/Selector/Selectors.tsx";
 
-function Base() {
-  return (
-    <HelmetProvider>
-      <>
-        <Metadata></Metadata>
-        <div className="flex flex-col h-screen">
-          <NavigationBar/>
-          <div className="justify-center items-center">
-            <div className="flex justify-between w-full max-w-screen-lg mx-auto">
-              <Selectors/>
-            </div>
-            <div className="flex pt-4">
-              <Cards/>
-            </div>
-          </div>
-          <div className="flex justify-center items-center fixed bottom-0 w-full pb-4 bg-dark">
-            <Pagination showShadow color="secondary" total={10} initialPage={1}/>
-          </div>
-        </div>
-      </>
-    </HelmetProvider>
-  )
-}
 
 function App() {
   const darkMode = useDarkMode(true);
