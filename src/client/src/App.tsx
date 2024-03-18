a21e4f3be3e3b26d07b5040cd9db31887df7f4eb
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-18 02:54:45 +0100

    added og tags

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -3,14 +3,26 @@ import { Helmet } from 'react-helmet';
 
 function App() {
   const baseUrl = window.location.origin;
+  const currentUrl = window.location.href;
   const imageUrl = `${baseUrl}/static/global/open-graph.jpeg`;
+  const brandName: string = 'Vocabulary'
+  const description: string = 'A simple and easy way to learn vocabulary.'
 
   return (
     <>
       <Helmet>
-        <meta property="og:title" content="Your page title"/>
-        <meta property="og:description" content="Your page description"/>
+        <meta property="og:url" content={baseUrl}/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content={brandName}/>
+        <meta property="og:description" content={description}/>
         <meta property="og:image" content={imageUrl}/>
+
+        <meta name="twitter:card" content={imageUrl}/>
+        <meta property="twitter:domain" content={baseUrl}/>
+        <meta property="twitter:url" content={currentUrl}/>
+        <meta name="twitter:title" content={brandName}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={imageUrl}/>
       </Helmet>
       <div>
         <h1>Welcome</h1>
