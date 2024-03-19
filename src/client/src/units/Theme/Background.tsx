d01633c9f1ac3877e51f518f4075be9f744ca44b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 22:37:18 +0100

    added themes for background colors

--- src/client/src/units/Theme/Background.tsx
+++ src/client/src/units/Theme/Background.tsx
@@ -5,7 +5,11 @@ interface BackgroundProps {
 }
 
 const Background: React.FC<BackgroundProps> = ({ isDarkMode }) => {
-  const bgClasses = 'bg-gradient-to-tr from-sky-950 via-black to-purple-950 from-5% via-60%'
+  const themes = {
+    'blackToPurple': 'bg-gradient-to-tr from-black via-sky-950 to-purple-950 from-50%',
+    'blueToPurple': 'bg-gradient-to-tr from-sky-950 via-black to-purple-950 from-5% via-60%',
+  }
+  const bgClasses = themes.blueToPurple
   const bgColor = isDarkMode ? bgClasses : '';
   return (
     <div className={`fixed inset-0 ${bgColor}`}></div>
