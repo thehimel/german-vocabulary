a40892a687e36df22be83666ce91b2bd8b5fd473
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 22:33:20 +0100

    updated background color

--- src/client/src/units/Theme/Background.tsx
+++ src/client/src/units/Theme/Background.tsx
@@ -5,7 +5,7 @@ interface BackgroundProps {
 }
 
 const Background: React.FC<BackgroundProps> = ({ isDarkMode }) => {
-  const bgClasses = 'bg-gradient-to-tr from-black via-sky-950 to-purple-950 from-50%'
+  const bgClasses = 'bg-gradient-to-tr from-sky-950 via-black to-purple-950 from-5% via-60%'
   const bgColor = isDarkMode ? bgClasses : '';
   return (
     <div className={`fixed inset-0 ${bgColor}`}></div>
