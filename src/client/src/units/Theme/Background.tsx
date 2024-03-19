62e079ddf5809418d0b792ba3a5823ec19e78cbb
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 19:06:47 +0100

    fixed background appearance in mobile devices

--- src/client/src/units/Theme/Background.tsx
+++ src/client/src/units/Theme/Background.tsx
@@ -3,7 +3,7 @@ import useDarkMode from "use-dark-mode";
 function Background() {
   const darkMode = useDarkMode();
   return (
-    <div className={`fixed inset-0 ${darkMode.value ? 'bg-gradient-to-bl from-purple-950 to-transparent' : ''}`}></div>
+    <div className={`fixed inset-0 ${darkMode.value ? 'bg-gradient-to-bl from-purple-950 to-black' : ''}`}></div>
   );
 }
 
