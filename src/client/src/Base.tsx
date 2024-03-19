23ae5b676cf31c0644f5a540eeb558abdbf7110d
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-19 18:38:51 +0100

    fixed view alignments, added z-index for pagination, added bg-gradient

--- src/client/src/Base.tsx
+++ src/client/src/Base.tsx
@@ -3,21 +3,29 @@ import Cards from './units/Cards/Cards.tsx'
 import NavigationBar from "./units/NavigationBar/NavigationBar.tsx";
 import {Pagination} from "@nextui-org/react";
 import Selectors from "./units/Selector/Selectors.tsx";
+import useDarkMode from "use-dark-mode";
 
 function Base() {
+  const darkMode = useDarkMode();
+
   return (
-    <div className="flex flex-col">
-      <NavigationBar/>
-      <div className="flex flex-col min-h-screen justify-center items-center">
-        <div className="flex justify-between w-full max-w-screen-lg mx-auto gap-2 pt-2 ps-2 pe-2">
-          <Selectors/>
+    <div className="relative">
+      <div className={`fixed inset-0 ${darkMode.value ? 'bg-gradient-to-bl from-purple-950 to-transparent' : ''}`}></div>
+      <div className="flex flex-col h-screen">
+        <NavigationBar/>
+        <div className="flex flex-col h-screen justify-center items-center">
+          <div className="flex justify-between w-full max-w-screen-lg mx-auto gap-2 pt-2 ps-2 pe-2">
+            <Selectors/>
+          </div>
+          <div className="flex justify-center items-center pb-12">
+            <Cards/>
+          </div>
         </div>
-        <div className="flex justify-center items-center pb-12">
-          <Cards/>
+        <div className="fixed bottom-0 w-full bg-dark z-10">
+          <div className="flex justify-center items-center pb-2 pt-2">
+            <Pagination isCompact showControls total={10} initialPage={1} color="secondary" size="lg"/>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center fixed bottom-0 w-full pb-2 pt-2 bg-dark">
-        <Pagination isCompact showControls total={10} initialPage={1} color="secondary" size="lg" />
       </div>
     </div>
   )
