6b1d78921fa8be0084cea1c64e9b8718859f607f
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-20 23:34:30 +0100

    configured api url in server proxy

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_URL = process.env.API_URL || 'http://127.0.0.1:8000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +11,13 @@ export default defineConfig({
   },
   define: {
     "global": {},  // for use-dark-mode
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: API_URL,
+        changeOrigin: true,
+      }
+    }
   },
 })
