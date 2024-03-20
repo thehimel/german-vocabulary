856e5756d82e0af140777cb874c69a1aadb54a4f
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-20 18:42:29 +0100

    updated TranslateAPIView

--- src/apps/trans/views.py
+++ src/apps/trans/views.py
@@ -6,8 +6,8 @@ from apps.trans.utils import translator
 
 class TranslateAPIView(APIView):
     @staticmethod
-    def post(request):
-        data = request.data
+    def get(request):
+        data = request.query_params
         text = data.get("text", "")
         src = data.get("source_language_code", "auto")
         dest = data.get("target_language_code", "en")
