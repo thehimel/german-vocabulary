b901c7d0e6f7824e77ee74bdc133d9e0ffa7e7b5
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-21 20:42:54 +0100

    added WordActions

---
+++ src/client/src/store/WordActions.ts
@@ -0,0 +1,24 @@
+import {WordActions} from "./WordSlice.ts";
+import axios from "axios";
+import {Dispatch} from "@reduxjs/toolkit";
+
+export const fetchWords = () => {
+  return async (dispatch: Dispatch) => {
+    const fetchData = async () => {
+    const response = await axios.get('/api/words/');
+    if (!response.data) {
+        throw new Error('Could not fetch cart data!');
+      }
+    return response;
+  };
+
+  try {
+    const words = await fetchData();
+    dispatch(
+      WordActions.updateWords(words)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  };
+};
