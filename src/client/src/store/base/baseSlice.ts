8ddb42a651b58aacaa4858599cead0f1ceb4087b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-22 19:08:39 +0100

    added interface in base slice

--- src/client/src/store/base/baseSlice.ts
+++ src/client/src/store/base/baseSlice.ts
@@ -1,11 +1,18 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {slices} from "../constants.ts";
 
+interface BaseState {
+  isDarkMode: boolean;
+}
+
+// Define the initial state using that type
+const initialState: BaseState = {
+  isDarkMode: true,
+}
+
 const baseSlice = createSlice({
   name: slices.base,
-  initialState: {
-    isDarkMode: true,
-  },
+  initialState,
   reducers: {
     updateDarkMode(state, action): void {
       state.isDarkMode = action.payload;
