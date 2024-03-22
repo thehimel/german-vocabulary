390c3b390567ec353908bc86eb3acee6a0681a8c
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-22 21:55:24 +0100

    updated wordsSlice.ts

--- src/client/src/store/words/wordsSlice.ts
+++ src/client/src/store/words/wordsSlice.ts
@@ -1,7 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {slices} from "../constants.ts";
 
-type Language = "de" | "en" | "bn";
+export type Language = "de" | "en" | "bn";
 
 export interface WordsStateProps {
   words: [];
