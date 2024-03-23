3ad0c8e940265b6e432482aa956e2bb4b0d0fea5
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-23 05:19:27 +0100

    connect searchBar to store

--- src/client/src/units/NavigationBar/SearchBar.tsx
+++ src/client/src/units/NavigationBar/SearchBar.tsx
@@ -1,11 +1,23 @@
 import {SearchIcon} from "./SearchIcon.tsx";
 import {Input} from "@nextui-org/react";
+import {AppDispatch} from "../../store/store.ts";
+import {useAppDispatch, useAppSelector} from "../../store/hooks.ts";
+import {ChangeEvent} from "react";
+import {fetchWords} from "../../store/words/wordsActions.ts";
 
 interface SearchBarProps {
   size?: string;
 }
 
 export const SearchBar = ({size = "20rem",}: SearchBarProps) => {
+  const dispatch: AppDispatch = useAppDispatch();
+  const primaryLanguage = useAppSelector((state) => state.base.primaryLanguage);
+  const level = useAppSelector((state) => state.base.level);
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    dispatch(fetchWords(primaryLanguage, level, value ? value : ""));
+  };
+
   return (
     <Input
       classNames={{
@@ -18,6 +30,7 @@ export const SearchBar = ({size = "20rem",}: SearchBarProps) => {
       size="sm"
       startContent={<SearchIcon size={18} />}
       type="search"
+      onChange={handleSearch}
     />
   );
 }
