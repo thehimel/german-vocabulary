ff87a0993b9bd4e3da4ade0b3f080ae18c5421ab
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-23 05:09:23 +0100

    added searchQuery as a parameter in fetchWords

--- src/client/src/store/words/wordsActions.ts
+++ src/client/src/store/words/wordsActions.ts
@@ -5,13 +5,16 @@ import {WORDS_API_URL} from "../constants.ts";
 import {getErrorMessage} from "../handleError.ts";
 
 
-export const fetchWords = (primaryLanguage: string, level: string) => {
+export const fetchWords = (primaryLanguage: string, level: string, searchQuery?: string) => {
   return async (dispatch: AppDispatch) => {
     try {
       dispatch(wordsActions.setWordsLoading());
-      const response = await axios.get(WORDS_API_URL, {
-        params: {primary_language: primaryLanguage, level: level}
-      });
+      const params: Record<string, string> = {
+        primary_language: primaryLanguage,
+        level: level,
+        ...(searchQuery ? { q: searchQuery } : {})
+      };
+      const response = await axios.get(WORDS_API_URL, {params: params});
       dispatch(wordsActions.setWords(response.data));
     } catch (error) {
       const errorMessage = getErrorMessage(WORDS_API_URL, error as AxiosError)
