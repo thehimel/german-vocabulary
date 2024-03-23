6092433d68e86ea2c1b6d9341eec03ff848e5a6c
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-23 15:41:45 +0100

    moved contents of Base inside App

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,6 +1,8 @@
-import Base from "./Base.tsx";
 import Metadata from "./units/Metadata/Metadata.tsx";
 import {useAppSelector} from "./store/hooks.ts";
+import Background from "./units/Theme/Background.tsx";
+import NavigationBar from "./units/NavigationBar/NavigationBar.tsx";
+import Home from "./units/Screens/Home.tsx";
 
 
 function App() {
@@ -9,7 +11,16 @@ function App() {
   return (
     <main className={`${darkMode ? 'dark' : ''} text-foreground bg-background`}>
       <Metadata/>
-      <Base/>
+      <div className="relative">
+        <Background/>
+        <div className="flex flex-col h-screen">
+          <NavigationBar/>
+          <div className="flex flex-col flex-grow bg-background">
+            <Home/>
+          </div>
+          {/* <PaginationBar/> */}
+        </div>
+      </div>
     </main>
   );
 }
