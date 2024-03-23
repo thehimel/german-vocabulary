d2c563311ab06220e48c350127c55531c2acdf1f
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-23 05:14:37 +0100

    updated search bar

--- src/client/src/units/NavigationBar/SearchBar.tsx
+++ src/client/src/units/NavigationBar/SearchBar.tsx
@@ -5,19 +5,21 @@ interface SearchBarProps {
   size?: string;
 }
 
-export const SearchBar = ({size = "20rem",}: SearchBarProps) => (
-  <Input
-    classNames={{
-      base: `max-w-full sm:max-w-[${size}] h-10`,
-      mainWrapper: "h-full",
-      input: "text-small",
-      inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-    }}
-    placeholder="Type to search..."
-    size="sm"
-    startContent={<SearchIcon size={18} />}
-    type="search"
-  />
-);
+export const SearchBar = ({size = "20rem",}: SearchBarProps) => {
+  return (
+    <Input
+      classNames={{
+        base: `max-w-full sm:max-w-[${size}] h-10`,
+        mainWrapper: "h-full",
+        input: "text-small",
+        inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+      }}
+      placeholder="Type to search..."
+      size="sm"
+      startContent={<SearchIcon size={18} />}
+      type="search"
+    />
+  );
+}
 
 export default SearchBar;
