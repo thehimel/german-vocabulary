3227131928c33f2ffad51ffd58a7c505c2870fb9
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-24 04:49:08 +0100

    updated PaginationBar

--- src/client/src/units/NavigationBar/PaginationBar.tsx
+++ src/client/src/units/NavigationBar/PaginationBar.tsx
@@ -1,9 +1,18 @@
 import {Pagination} from "@nextui-org/react";
+import {FC} from "react";
+import {useAppSelector} from "../../store/hooks.ts";
 
-function PaginationBar() {
+interface PaginationBarProps {
+  initialPage: number;
+  total: number;
+}
+
+const PaginationBar: FC<PaginationBarProps> = ({initialPage, total}) => {
+  const darkMode = useAppSelector((state) => state.base.darkMode);
+
   return (
-    <div className="flex fixed bottom-0 w-full z-10 justify-center items-center pb-2 pt-2">
-      <Pagination isCompact showControls total={10} initialPage={1} color="secondary" size="lg"/>
+    <div className={`flex fixed bottom-0 w-full z-50 justify-center pb-2 pt-2 ${darkMode ? 'dark' : ''}`}>
+      <Pagination isCompact showControls total={total} initialPage={initialPage} color="secondary" size="lg"/>
     </div>
   );
 }
