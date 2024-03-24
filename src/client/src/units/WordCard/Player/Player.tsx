7c884bbde113be382f4443d7815f36138c932af1
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-24 08:35:18 +0100

    changed icons in Player

--- src/client/src/units/WordCard/Player/Player.tsx
+++ src/client/src/units/WordCard/Player/Player.tsx
@@ -1,11 +1,12 @@
 import { FC, useRef, useState } from "react";
 import axios from "axios";
 import { TTS_API_URL } from "../../../store/constants.ts";
-import {SpeakerLoudIcon, SpeakerOffIcon} from "@radix-ui/react-icons";
 import {AppDispatch} from "../../../store/store.ts";
 import {useAppDispatch, useAppSelector} from "../../../store/hooks.ts";
 import {setIsPlayingGlobal} from "../../../store/base/baseActions.ts";
+import SpeakerPlayIcon from "./icons/SpeakerPlayIcon.tsx";
 import Spinner from "./icons/Spinner.tsx";
+import SpeakerStopIcon from "./icons/SpeakerStopIcon.tsx";
 
 interface Props {
   text: string;
@@ -85,8 +86,8 @@ const Player: FC<Props> = ({ text, language }) => {
     // If a button is pressed, the global isPlaying turns on. Therefore, all buttons becomes disabled.
     // When a button is connected to the current audio, it should not be disabled, therefore it is connected to
     // disabled=isButtonDisabled. If the button is not connected to the current audio, then disabled=isPlayingGlobal.
-    <button className="ms-1" onClick={handleClick} disabled={isPlaying ? isButtonDisabled : isPlayingGlobal}>
-      {isPlaying ? <SpeakerOffIcon /> : isLoading ? <Spinner/> : <SpeakerLoudIcon />}
+    <button onClick={handleClick} disabled={isPlaying ? isButtonDisabled : isPlayingGlobal}>
+      {isPlaying ? <SpeakerStopIcon /> : isLoading ? <Spinner/> : <SpeakerPlayIcon/>}
     </button>
   );
 };
