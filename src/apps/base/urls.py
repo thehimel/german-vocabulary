762b94a16c945e3b7bc97ec9473833c1935022b0
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-24 05:40:41 +0100

    fix home view in django frontend

--- src/apps/base/urls.py
+++ src/apps/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.base.views.base import MessageView
+from apps.base.views.base import MessageView, HomeView
 from apps.base.views.client import ClientView
 from apps.base.views.get_started import GetStartedWizard
 from apps.base.views.languages import LanguagePreferencesView
@@ -9,6 +9,7 @@ app_name = "base"
 
 urlpatterns = [
     path("", ClientView.as_view(), name="client"),
+    path("home/", HomeView.as_view(), name="home"),
     path("messages/", MessageView.as_view(), name="messages"),
     path("language-preferences/", LanguagePreferencesView.as_view(), name="language_preferences"),
     path("get-started/", GetStartedWizard.as_view(), name="get_started"),
