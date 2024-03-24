34bacff3bf6c372fa4863f657b421506f90219f3
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-24 05:24:25 +0100

    updated bottom padding PaginationBar

--- src/client/src/units/NavigationBar/PaginationBar.tsx
+++ src/client/src/units/NavigationBar/PaginationBar.tsx
@@ -12,7 +12,7 @@ const PaginationBar: FC<PaginationBarProps> = ({initialPage, total, onChange}) =
   const darkMode = useAppSelector((state) => state.base.darkMode);
 
   return (
-    <div className={`flex fixed bottom-0 w-full z-50 justify-center pb-2 pt-2 ${darkMode ? 'dark' : ''}`}>
+    <div className={`flex fixed bottom-0 w-full z-50 justify-center pb-8 pt-2 ${darkMode ? 'dark' : ''}`}>
       <Pagination isCompact showControls showShadow color="secondary" size="lg" total={total} initialPage={initialPage} onChange={onChange}/>
     </div>
   );
