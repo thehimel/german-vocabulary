e703214516caeaffbc6a258a3e205b2a891cab40
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-25 22:25:14 +0100

    updated urls.py for api

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -1,13 +1,8 @@
 from django.urls import include, path
 
-from apps.api.views import HomeAPIView
-from apps.speech.views import TextToSpeechAPIView
-from apps.trans.views import TranslateAPIView
-
 app_name = "api"
 
 urlpatterns = [
-    path("", HomeAPIView.as_view(), name="home"),
     path("words/", include("apps.words.urls", namespace="words")),
     path("translate/", include("apps.trans.urls", namespace="trans")),
     path("text-to-speech/", include("apps.speech.urls", namespace="speech")),
