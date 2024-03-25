ca681b16d49400ed4073245abce925397fcd51eb
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-25 06:46:26 +0100

    reformat PaginationBar.tsx

--- src/client/src/units/NavigationBar/PaginationBar.tsx
+++ src/client/src/units/NavigationBar/PaginationBar.tsx
@@ -13,7 +13,16 @@ const PaginationBar: FC<PaginationBarProps> = ({initialPage, total, onChange}) =
 
   return (
     <div className={`flex fixed bottom-0 w-full z-50 justify-center pb-8 pt-2 ${darkMode ? 'dark' : ''}`}>
-      <Pagination isCompact showControls showShadow color="secondary" size="lg" total={total} initialPage={initialPage} onChange={onChange}/>
+      <Pagination
+        isCompact
+        showControls
+        showShadow
+        color="secondary"
+        size="lg"
+        total={total}
+        initialPage={initialPage}
+        onChange={onChange}
+      />
     </div>
   );
 }
