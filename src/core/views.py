166ea073cbac970c7359cb5c00ce392dcc4e4b43
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-25 22:23:02 +0100

    updated settings for drf-yasg

--- src/core/views.py
+++ src/core/views.py
@@ -1,12 +1,13 @@
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from apps.base.constants import BRAND_NAME
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Vocabulary API",
+        title=BRAND_NAME,
         default_version="v1",
-        description="A simple and easy way to learn German vocabulary.",
+        description="A simple and easy way to learn vocabulary.",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
