9100bfa8821c2d2dff8e38ff5b377efd8e417b03
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-27 06:24:03 +0100

    updated TextAvatar

--- src/client/src/units/Avatar/TextAvatar.tsx
+++ src/client/src/units/Avatar/TextAvatar.tsx
@@ -1,12 +1,7 @@
 import {FC} from "react";
 import {Avatar} from "@nextui-org/react";
 import {useAppSelector} from "../../store/hooks.ts";
-
-const bgColors: Record<string, string> = {
-  "de": "bg-amber-500",
-  "en": "bg-indigo-700",
-  "bn": "bg-emerald-500",
-};
+import {getLanguageStyle} from "../utils/utils.ts";
 
 export interface LanguageIconProps {
   text: string;
@@ -16,8 +11,8 @@ export interface LanguageIconProps {
 
 const TextAvatar: FC<LanguageIconProps> = ({text, textColor, animate}) => {
   const darkMode = useAppSelector((state) => state.base.darkMode);
-  const color = textColor ? textColor: "text-gray-50";
-  const bgColor = bgColors[text.toLowerCase()] || "bg-purple-500";
+  const color = textColor ? textColor: 'text-gray-50';
+  const bgColor = getLanguageStyle(text, 'bg');
   const animation = animate ? "animate-pulse" : null
   return (
     <Avatar isBordered alt={text} className={`${darkMode ? 'dark': ''} ${bgColor} ${color} ${animation} w-6 h-6 mt-0.5`} name={text.toUpperCase()} />
--- src/client/src/units/utils/utils.ts
+++ src/client/src/units/utils/utils.ts
@@ -3,3 +3,14 @@ import {SelectorChoice} from "../../store/base/baseSlice.ts";
 export const filterLanguageChoices = (choices: SelectorChoice[], exclude_language: string): SelectorChoice[] => {
   return choices.filter(choice => choice.key !== exclude_language);
 };
+
+const languageStyles: Record<string, Record<string, string>> = {
+    'de': { bg: 'bg-amber-500', shadow: 'shadow-amber-500' },
+    'en': { bg: 'bg-indigo-700', shadow: 'shadow-indigo-700' },
+    'bn': { bg: 'bg-emerald-500', shadow: 'shadow-emerald-500' }
+};
+
+export const getLanguageStyle = (code: string, type: 'bg' | 'shadow') => {
+    const languageCode = code.toLowerCase();
+    return languageStyles[languageCode]?.[type] || (type === 'bg' ? 'bg-purple-500' : 'shadow-purple-500');
+};
