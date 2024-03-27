b040adabb3bdcf9d1abcbde0fc28a3b260c9a576
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-27 04:54:36 +0100

    updated background color for Word skeleton

--- src/client/src/units/Words/Skeletons/WordSkeleton.tsx
+++ src/client/src/units/Words/Skeletons/WordSkeleton.tsx
@@ -2,7 +2,7 @@ import {Card, Divider, Skeleton} from "@nextui-org/react";
 
 export default function WordSkeleton() {
   return (
-    <Card className="shadow-sm shadow-purple-500 space-y-5 p-4" radius="lg">
+    <Card className="shadow-sm shadow-purple-500 dark:bg-zinc-800 space-y-5 p-4" radius="lg">
       <div className="flex justify-center">
         <Skeleton className="rounded-lg w-2/5 h-4 bg-default-300"/>
       </div>
