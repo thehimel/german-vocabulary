de1beaf898baa4bbb2bc48dafc9dd647fdd27316
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-27 23:19:06 +0100

    make articles reactive to parts of speech

--- src/client/src/units/Screens/WordInput.tsx
+++ src/client/src/units/Screens/WordInput.tsx
@@ -1,5 +1,5 @@
 import {Card, CardBody, CardHeader, Input} from "@nextui-org/react";
-import {FC} from "react";
+import {ChangeEvent, FC, useEffect, useState} from "react";
 import {Language, levelChoices, SelectorChoice} from "../../store/base/baseSlice.ts";
 import Selector from "../Selectors/Selector.tsx";
 import {getLanguageStyle, getSelectorChoices} from "../utils/utils.ts";
@@ -13,14 +13,19 @@ interface WordInputProps {
 
 const WordInput: FC<WordInputProps> = ({ language, partsOfSpeech}) => {
   const shadowColor = `flex flex-wrap gap-2 pt-2 shadow-sm ${getLanguageStyle(language.code, 'shadow')}`;
+  const [partOfSpeech, setPartOfSpeech] = useState('');
+
+  useEffect(() => {
+    if (partsOfSpeech && partsOfSpeech.length > 0) {
+      setPartOfSpeech(partsOfSpeech[0].key);
+    }
+  }, [partsOfSpeech]);
+
+  const handlePartOfSpeechChange = (e: ChangeEvent<HTMLSelectElement>) => setPartOfSpeech(e.target.value);
+
   const articles = getSelectorChoices(language.articles);
-  const articlesComponent = articles && articles[0]?.key ? (
-    <Selector
-      label="Articles"
-      defaultKey={articles[0].key}
-      choices={articles}
-      onChange={() => null}
-    />
+  const articlesComponent = partOfSpeech.toLowerCase() === 'noun' && articles && articles[0]?.key ? (
+    <Selector label="Articles" defaultKey={articles[0].key} choices={articles} onChange={() => null}/>
   ) : null;
 
   return (
@@ -32,7 +37,7 @@ const WordInput: FC<WordInputProps> = ({ language, partsOfSpeech}) => {
         <div className="flex flex-wrap gap-2">
           <Input required type="text" label="Word"/>
           <Selector label="Level" defaultKey={levelChoices[0].key} choices={levelChoices} onChange={() => null}/>
-          {partsOfSpeech?.length && <Selector label="Parts of Speech" defaultKey={partsOfSpeech[0].key} choices={partsOfSpeech} onChange={() => null}/>}
+          {partsOfSpeech?.length && <Selector label="Parts of Speech" defaultKey={partsOfSpeech[0].key} choices={partsOfSpeech} onChange={handlePartOfSpeechChange}/>}
           { articlesComponent }
           <Input type="text" label="Plural"/>
           <Input type="text" label="Sentence"/>
