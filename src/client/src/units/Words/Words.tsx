3fdeaa158b513ad6e21fd5f6e20397212af6d99d
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-27 23:48:38 +0100

    fix flex view for Bengali in Words

--- src/client/src/units/Words/Words.tsx
+++ src/client/src/units/Words/Words.tsx
@@ -12,7 +12,7 @@ const Words: FC<WordsProps> = ({words}) => {
   return (
     <div className="flex justify-center max-w-screen-xl mx-auto pt-2 ps-2 pe-2">
       <div
-        className={`${size > 4 ? 'grid grid-cols-2 sm:grid-cols-4' : size > 2 ? 'grid grid-cols-2' : 'flex'} gap-2`}
+        className={`${size > 3 ? 'grid grid-cols-2 sm:grid-cols-4 w-full' : size > 2 ? 'grid grid-cols-2' : 'flex'} gap-2`}
       >
         {words.map((item, index) => (
           <Word key={index} index={index} word={item}/>
