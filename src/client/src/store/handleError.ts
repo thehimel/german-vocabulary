443be017f291d6db0f0beba201f01bf477c69f97
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-30 11:39:45 +0100

    updated getErrorMessage()

--- src/client/src/store/handleError.ts
+++ src/client/src/store/handleError.ts
@@ -6,12 +6,15 @@ interface AxiosErrorResponseData {
 }
 
 export const getErrorMessage = ({apiUrl, error} : {apiUrl: string, error: AxiosError}) => {
-  let errorMessage = "";
+  let errorMessage = {};
   if (error.response) {
     const responseData = error.response.data as AxiosErrorResponseData;
-    errorMessage = responseData.message || 'An error occurred';
+    errorMessage = responseData.message || {errors: ['An error occurred']};
   } else {
-    errorMessage = error.message || 'An unknown error occurred';
+    errorMessage = error.message || {errors: ['An unknown error occurred']};
   }
-  return `${apiUrl}: ${errorMessage}`
+  return {
+    apiUrl: apiUrl,
+    ...errorMessage
+  }
 };
