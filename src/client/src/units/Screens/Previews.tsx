b4d665304604261d9b36e84d9e8a8c4ea321cd03
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-30 17:48:34 +0100

    automatically clear the message after an interval

--- src/client/src/units/Screens/Previews.tsx
+++ src/client/src/units/Screens/Previews.tsx
@@ -1,5 +1,5 @@
 import {useEffect} from "react";
-import {fetchPreviews} from "../../store/previews/previewsActions.ts";
+import {fetchPreviews, setPreviewsMessage} from "../../store/previews/previewsActions.ts";
 import {AppDispatch} from "../../store/store.ts";
 import {useAppDispatch, useAppSelector} from "../../store/hooks.ts";
 import Alert from "../Alert/Alert.tsx";
@@ -14,6 +14,7 @@ const Home = () => {
   const primaryLanguage = useAppSelector((state) => state.base.primaryLanguage);
   const level = useAppSelector((state) => state.base.level);
   const loading = useAppSelector((state) => state.previews.loading);
+
   useEffect(() => {
     dispatch(fetchPreviews({
       language: primaryLanguage,
@@ -21,6 +22,18 @@ const Home = () => {
       loader: true
     }))
   }, [dispatch, level, primaryLanguage]);
+
+  useEffect(() => {
+    if (message) {
+      // Dispatch action to clear the message after a delay (e.g., 5 seconds)
+      const clearMessageTimeout = setTimeout(() => {
+        dispatch(setPreviewsMessage({message: ''}));
+      }, 2000);
+
+      // Clear the timeout when the component unmounts or when message changes
+      return () => clearTimeout(clearMessageTimeout);
+    }
+  }, [message, dispatch]);
 
   return (
     <>
