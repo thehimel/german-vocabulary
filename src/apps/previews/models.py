f8481c6dce130ff9161e6fe892980e403e3f4158
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-31 04:05:42 +0200

    add part of speech field in PreWord

--- src/apps/previews/models.py
+++ src/apps/previews/models.py
@@ -6,19 +6,22 @@ from apps.words.utils import getLevelChoices
 
 class PreWord(models.Model):
     title = models.CharField(max_length=100)
-    plural = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField(default="", blank=True, null=True)
     language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    part_of_speech = models.ForeignKey(
+        PartOfSpeech, verbose_name="Part of Speech", on_delete=models.SET_NULL, null=True, blank=True
+    )
     articles = models.ManyToManyField(Article, blank=True)
+    plural = models.CharField(max_length=100, blank=True, null=True)
     sentence = models.TextField(default="", blank=True, null=True)
     level = models.CharField(max_length=2, choices=getLevelChoices())
+    description = models.TextField(default="", blank=True, null=True)
     hidden = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Word"
-        unique_together = ["title", "language"]
+        unique_together = ["title", "language", "part_of_speech"]
 
     def __str__(self):
         return f"{self.title} | {self.language}"
