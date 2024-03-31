6d9dccca8c345c7783416be142b9ac9a9003995b
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-03-31 23:52:49 +0200

    updated error handler

--- src/client/src/store/handleError.ts
+++ src/client/src/store/handleError.ts
@@ -2,18 +2,13 @@ import {AxiosError} from "axios";
 import {ErrorInterface} from "./previews/previewsSlice.ts";
 
 
-interface AxiosErrorResponseData {
-  message: string[];
-}
-
 export const getErrorMessage = ({apiUrl, error} : {apiUrl: string, error: AxiosError}) => {
   const errorMessage: ErrorInterface = {
     apiUrl: apiUrl,
     errors: [],
   }
   if (error.response) {
-    const responseData = error.response.data as AxiosErrorResponseData;
-    errorMessage.errors = Array.isArray(responseData.message) ? responseData.message : [responseData.message || 'An error occurred'];
+    errorMessage.errors = [error.response.data as never]
   } else {
     errorMessage.errors = [error.message || 'An unknown error occurred'];
   }
