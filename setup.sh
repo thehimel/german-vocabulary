084369f5a4f226bd0fb79ee4025d43b92e84c841
thehimel <60639301+thehimel@users.noreply.github.com>
2024-08-20 23:33:43 +0600

    updated setup and build scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 cd src/client && npm install --force && npx vite build && cd ../..
-pip install --upgrade setuptools
-pip install --upgrade pip
+pip install --upgrade pip setuptools
 pip install -r src/requirements.txt
 python scripts/env_init.py
 ./migrate.sh
--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 cd client && npm install --force && npx vite build && rm -rf node_modules && cd ..
-python3.9 -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip setuptools
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input
 python3.9 manage.py migrate
