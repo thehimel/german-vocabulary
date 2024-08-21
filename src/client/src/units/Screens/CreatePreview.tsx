0a4c6e7fda1e27b35257632005207c5a572a5825
thehimel <60639301+thehimel@users.noreply.github.com>
2024-08-21 23:36:37 +0600

    client: refactored CreatePreview.tsx

--- src/client/src/units/Screens/CreatePreview.tsx
+++ src/client/src/units/Screens/CreatePreview.tsx
@@ -9,17 +9,8 @@ import CustomInput from "../Fields/CustomInput.tsx";
 import CustomSelect from "../Fields/CustomSelect.tsx";
 import SubmitButton from "../Fields/SubmitButton.tsx";
 import {toggleDarkModeStyleSheet} from "../Selectors/utils.ts";
+import {mapTranslationsToTWordSchema} from "../utils/utils.ts";
 import CreateWord from "./CreateWord.tsx";
-
-interface Translation {
-  language_code: string;
-  word: string;
-  level: string;
-  parts_of_speech: string;
-  article_singular: string;
-  plural: string;
-  sentence: string;
-}
 
 const CreatePreview = () => {
   const darkMode = useAppSelector((state) => state.base.darkMode);
@@ -38,6 +29,7 @@ const CreatePreview = () => {
   });
 
   const [wordValues, setWordValues] = useState<TPreviewSchema["words"]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (data: TPreviewSchema) => {
     console.log("Form data:", data);
@@ -46,17 +38,12 @@ const CreatePreview = () => {
 
   const fetchTranslations = async (word: string, languageCode: string) => {
     try {
+      setIsLoading(true);
       const response = await axios.post("/api/translate/word/", { word, language_code: languageCode });
-      const translations: TWordSchema[] = response.data.translations.map((translation: Translation) => ({
-        languageCode: translation.language_code,
-        title: translation.word,
-        level: translation.level,
-        partOfSpeech: translation.parts_of_speech,
-        article: translation.article_singular,
-        plural: translation.plural,
-        sentence: translation.sentence,
-      }));
+      const translations: TWordSchema[] = mapTranslationsToTWordSchema(response.data.translations);
+      setIsLoading(false);
       setWordValues(translations);
+      console.log(translations);
     } catch (error) {
       console.error("Error fetching translations:", error);
     }
@@ -69,8 +56,6 @@ const CreatePreview = () => {
       fetchTranslations(word, languageCode).then(() => null);
     }
   };
-
-  console.log(errors);
 
   return (
     <div className="flex justify-center max-w-screen-xl mx-auto pt-2 ps-2 pe-2">
@@ -100,7 +85,12 @@ const CreatePreview = () => {
               initialValues={wordValue}
             />
           ))}
-          <SubmitButton isDisabled={isSubmitting} isLoading={isSubmitting} title={"Submit"} color={"default"}/>
+          <SubmitButton
+            isDisabled={isSubmitting || isLoading}
+            isLoading={isSubmitting || isLoading}
+            title={wordValues?.length ? "Submit" : "Fetch Translations"}
+            color={"default"}
+          />
         </form>
       </div>
     </div>
--- src/client/src/units/utils/utils.ts
+++ src/client/src/units/utils/utils.ts
@@ -1,3 +1,4 @@
+import {TWordSchema} from "../../schemas/preview.ts";
 import {SelectorChoice} from "../../store/base/baseSlice.ts";
 
 export const filterLanguageChoices = (choices: SelectorChoice[], exclude_language: string): SelectorChoice[] => {
@@ -30,3 +31,25 @@ export const getCookie = (name: string) => {
   const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
   return cookieValue ? cookieValue.pop() : '';
 }
+
+export interface Translation {
+  language_code: string;
+  word: string;
+  level: string;
+  parts_of_speech: string;
+  article_singular: string;
+  plural: string;
+  sentence: string;
+}
+
+export const mapTranslationsToTWordSchema = (translations: Translation[]): TWordSchema[] => {
+  return translations.map((translation) => ({
+    languageCode: translation.language_code,
+    title: translation.word,
+    level: translation.level,
+    partOfSpeech: translation.parts_of_speech,
+    article: translation.article_singular || "",
+    plural: translation.plural || "",
+    sentence: translation.sentence || "",
+  }));
+};
