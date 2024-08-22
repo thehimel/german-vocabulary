28d79cbd40a49fd0c81ac71f840e1dd480b15079
thehimel <60639301+thehimel@users.noreply.github.com>
2024-08-22 16:11:49 +0600

    updated syncdb.sh

--- syncdb.sh
+++ syncdb.sh
@@ -1,7 +1,7 @@
 # Fetch data from PROD database and save to a file.
-./prod.sh && ./dumpdata.sh
+./set_prod.sh && ./dumpdata.sh
 echo "Fetched data from PROD database."
 
 # Insert data to DEV database from that file.
-./dev.sh && ./loaddata.sh
+./set_dev.sh && ./loaddata.sh
 echo "Inserted data to DEV database."
